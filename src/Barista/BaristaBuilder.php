7430128d66b082e7e785583aac5c4bc8bd8506f0
Gökhan Güney <gokhanguneygg@gmail.com>
2017-02-22 19:47:42 +0300

    Config and documentation.

--- src/Barista/BaristaBuilder.php
+++ src/Barista/BaristaBuilder.php
@@ -15,11 +15,26 @@ PUT/PATCH	/nerds/{id}			update				nerds.update
 DELETE		/nerds/{id}			destroy				nerds.destroy
 */
 class BaristaBuilder{
-
+    /**
+     * Reserved methods for form.
+     *
+     * @var array
+     */
 	protected static $RESERVED_METHODS = ['POST', 'PUT','PATCH', 'DELETE'];
+
+	/**
+     * @var array
+     */
 	protected static $RESERVED_ATTRIBUTES = [];
-	
-	public static function open($array )
+
+    /**
+     * Open a a new form.
+     *
+     * @param  array  $array
+     * 
+     * @return string
+     */
+	public static function open($array)
 	{
 		$method = strtoupper($array['method']);
 		$method = (in_array($method ,self::$RESERVED_METHODS))?$method :'POST';
@@ -39,38 +54,72 @@ class BaristaBuilder{
 		return '<form method="POST" action="'.$action.'" '.$files.'>'.csrf_field().method_field($method);
 	}
 
+	/**
+	 * Close a form.
+	 *
+	 * @param $attributes
+	 * 
+	 * @return string
+	 */
 	public static function close($attributes)
 	{
-		if(config('barista.shouldWrapElement'))
-			return '<div class="'.config('barista.elementWrapper').'"><button type="submit" class="'.$attributes['class'].'">'.$attributes['title'].'</button></div></form>';
+		if(!isset($attributes['class']))
+			$attributes['class'] = config('barista.btn_class').' '.config('barista.btn_primary').' '.config('barista.btn_additional_class');
+
+		if(config('barista.should_group_form'))
+			return '<div class="'.config('barista.form_group_class').'"><button type="submit" class="'.$attributes['class'].'">'.$attributes['title'].'</button></div></form>';
 		else 
 			return '<button type="submit" class="'.$attributes['class'].'">'.$attributes['title'].'</button></form>';
 	}
+
+	/**
+	 * Build Form From a DataModel
+	 * 
+	 * @param  DataModel $dataModel
+	 * @param  Model $item 
+	 * @param  array $errors    
+	 * 
+	 * @return string
+	 */
 	public static function buildFromDM($dataModel, $item = null, $errors)
 	{
 		$htmlFields = "";
 		$columns = $dataModel->getColumns();
 		$formFields = $dataModel->getFormFields();
-
+		$hiddenFields = $dataModel->getHiddenFields();
 		foreach ($formFields as $formField) {
-			if(isset( $columns[$formField]))
-				$column = $columns[$formField];
-
-			$attributes = $column->getAttributes();
-
-			$error = ($errors->has($formField))?'has-error':'';
-			$htmlFields .= '<div class="form-group '.$error.'">';
-			$htmlFields .= self::label($attributes['name'],$attributes['label']);
-
-			$htmlFields .= (isset($attributes['required']))?self::required($attributes['required']):null;
-			$htmlFields .= self::detectInput($dataModel, $attributes, $formField, $item);
-        	if ($errors->has($formField))
-				$htmlFields .=self::error($errors->first($formField), ['class'=>'help-block']);
-			$htmlFields .= '</div>';
+			if(!in_array($formField, $hiddenFields ))
+			{
+				if(isset( $columns[$formField]))
+					$column = $columns[$formField];
+				$attributes = $column->getAttributes();
+				$error = ($errors->has($formField)) ? ' '.config('barista.div_error_class') : '';
+				$htmlFields .= '<div class="'.config('barista.group_class').$error.'">';
+				$htmlFields .= '<div class="'.config('barista.group_label_class').'">';
+				$htmlFields .= self::label($attributes['name'], $attributes['label'], $attributes);
+				$htmlFields .= '</div>';
+				$htmlFields .= '<div class="'.config('barista.group_input_class').'">';
+				$htmlFields .= self::detectInput($dataModel, $attributes, $item);
+	        	if ($errors->has($formField))
+					$htmlFields .= self::error($errors->first($formField));
+				$htmlFields .= '</div>';
+				$htmlFields .= '</div>';
+			}			
 		}
 		return $htmlFields;
 	}
-	public static function detectInput($dataModel, $attributes, $formField, $item)
+
+	/**
+	 * Detect input type.
+	 * 
+	 * @param  DataModel $dataModel  
+	 * @param  array $attributes 
+	 * @param  string $formField  
+	 * @param  Model $item 
+	 * 
+	 * @return string            
+	 */
+	public static function detectInput($dataModel, $attributes, $item)
 	{
 		$foreigns = $dataModel->getForeigns();
 		$foreignsData = $dataModel->getForeignsData();
@@ -81,9 +130,7 @@ class BaristaBuilder{
 		if(isset($foreigns[$attributes['name']] ) && $foreignsData[$attributes['name']] )
 			$input .= self::select($name, $value, $foreignsData[$attributes['name']], $attributes);
 		else if($attributes['type'] == 'file')
-		{
 			$input .= self::file($name , $value, $attributes);
-		}
 		else if($attributes['maxlength'] > 255)
 			$input .= self::textarea($name , $value, $attributes);
 		else if($attributes['type'] == 'checkbox')
@@ -93,42 +140,59 @@ class BaristaBuilder{
 		
 		return $input;
 	}
+
+	/**
+	 * Build show partial from DataModel
+	 * 
+	 * @param  DataModel $dataModel
+	 * @param  Model $item    
+	 * 
+	 * @return string
+	 */
 	public static function buildShowFromDM($dataModel, $item)
 	{
 		$htmlFields = "";
 		$columns = $dataModel->getColumns();
 		$hiddenFields = $dataModel->getHiddenFields();
+
+		$groupClass = config('barista.show_group_class');
+		$groupLabelClass = config('barista.show_group_label_class');
+		$groupInputClass = config('barista.show_group_value_class');
+
 		foreach($columns as $key => $column)
 		{
-	
 			if(!in_array($column->get('name'), $hiddenFields ))
 			{
-				$value = ($item->$key != null)?$item->$key : '-';
 				$text = (\Lang::has('general.'.$column->get('label')))?trans('general.'.$column->get('label')):$column->get('label');
-				$htmlFields .= '<div class="col-md-4"><strong>'.$text.'</strong> </div>';
-				$htmlFields .= '<div class="col-md-8">';
+				$htmlFields .= '<div class="'.$groupClass.'">';
+				$htmlFields .= '<div class="'.$groupLabelClass.'">';
+				$htmlFields .= self::label($text,$text);
+				$htmlFields .= '</div>';
+				$htmlFields .= '<div class="'.$groupInputClass.'">';
+				$value = ($item->$key != null)?$item->$key : '-';
 				if( $column->get('type') == 'file' )
 				{
 					$file = (str_contains($value, ['http', 'https']))?$value:Storage::url($value);
-					$htmlFields .= '<img src="'.$file.'" style="width:300px;height:auto"/img>';
+					$htmlFields .= '<img src="'.$file.'" style="width:200px;height:auto"/img>';
 				}
 				else
-					$htmlFields .= e($value);
+					$htmlFields .= '<span>'.e($value).'</span>';
+				$htmlFields .= '</div>';
 				$htmlFields .= '</div>';
 			}
 		}
 		return $htmlFields;
 	}
+
+
 	public static function buildTableFromDM($dataModel, $items)
 	{
 		$htmlFields = "";
-		$columns = $dataModel->getColumns();
-		$tableFields = $dataModel->getTableFields();
 
-		$prefixWithSlash = ( config('app.admin_prefix') !==null && config('app.admin_prefix') != '')?'/'.config('app.admin_prefix').'/':'';
-		$prefix = ( config('app.admin_prefix') !==null && config('app.admin_prefix') != '')?config('app.admin_prefix'):'';
-		$htmlFields .='<table class="table compact" id="myTable">';
-		$htmlFields .='<thead class="thead-inverse">';
+		$prefixWithSlash = ( config('barista.prefix') !== null && config('barista.prefix') != '')?'/'.config('barista.prefix').'/':'';
+		$prefix = ( config('barista.prefix') !== null && config('barista.prefix') != '')?config('barista.prefix'):'';
+		$htmlFields .='<table class="'.config('barista.table_class').'" id="myTable">';
+		$htmlFields .='<thead class="'.config('barista.thead_class').'">';
 
 		$editText = (\Lang::has('general.Edit'))?trans('general.Edit'):'Edit';
 		$deleteText = (\Lang::has('general.Delete'))?trans('general.Delete'):'Delete';
@@ -137,6 +201,12 @@ class BaristaBuilder{
 		$showText = (\Lang::has('general.Show'))?trans('general.Show'):'Show';
 		$actionsText = (\Lang::has('general.Actions'))?trans('general.Actions'):'Actions';
 
+		$editButtonClasses = config('barista.tbl_btn_class').' '.config('barista.tbl_btn_primary').' '.config('barista.tbl_btn_sm_class').' '.config('barista.tbl_btn_additional_class');
+		$showButtonClasses = config('barista.tbl_btn_class').' '.config('barista.tbl_btn_info').' '.config('barista.tbl_btn_sm_class').' '.config('barista.tbl_btn_additional_class');
+		$deleteButtonClasses = config('barista.tbl_btn_class').' '.config('barista.tbl_btn_danger').' '.config('barista.tbl_btn_sm_class').' '.config('barista.tbl_btn_additional_class');
+
+		$columns = $dataModel->getColumns();
+		$tableFields = $dataModel->getTableFields();
 
 		foreach($tableFields as $tableField)
 		{
@@ -152,36 +222,54 @@ class BaristaBuilder{
 		foreach($items as $item)
 		{
 
-			$htmlFields .= '<tr >';
+			$htmlFields .= '<tr>';
 			foreach ($tableFields as $tableField)
 			{
 				$columnName = $columns[$tableField]->get('name');
 				$value = $item->$columnName;
 				$htmlFields .= '<td>'.e($value).'</td>';
-
 			}
 			$htmlFields .= '<td class="td w-clearfix" >';
-			$htmlFields .= '<a class="btn btn-primary btn-sm pull-left" style="margin-right:6px" href="'.$prefixWithSlash.lcfirst($dataModel->getName()).'/'.$item->id.'/edit">'.$editText.'</a>';
-			$htmlFields .= '<a class="btn btn-success btn-sm pull-left" style="margin-right:6px" href="'.$prefixWithSlash.lcfirst($dataModel->getName()).'/'.$item->id.'">'.$showText.'</a>';
+			$htmlFields .= '<a class="'.$editButtonClasses.' pull-left" href="'.$prefixWithSlash.lcfirst($dataModel->getName()).'/'.$item->id.'/edit">'.$editText.'</a>';
+			$htmlFields .= '<a class="'.$showButtonClasses.' pull-left" href="'.$prefixWithSlash.lcfirst($dataModel->getName()).'/'.$item->id.'">'.$showText.'</a>';
 			$htmlFields .= self::open([ 'method'=>'DELETE', 'item'=>$item, 'url'=>$prefix.'/'.lcfirst($dataModel->getName()) ]);
-			$htmlFields .= self::close(['class'=>'btn btn-danger btn-sm"', 'title'=>$deleteText]);
+			$htmlFields .= '<button type="submit" class="'.$deleteButtonClasses.'">'.$deleteText.'</button></form>';
 			$htmlFields .= '</tr>';
 		}	
 		$htmlFields .= '</tbody>';
-
 		$htmlFields .= '</table>';
 
 		return $htmlFields;
 	}
-	public static function checkbox ($name, $value, $attributes = null)
+
+	/**
+	 * Generate an HTML checkbox element.
+	 * 
+	 * @param  string $name       
+	 * @param  string $value   
+	 * @param  array $attributes
+	 * 
+	 * @return string
+	 */
+	public static function checkbox($name, $value, $attributes = null)
 	{
 		if(!isset($attributes['class']))
-			$attributes['class'] = 'checkbox';
+			$attributes['class'] = config('barista.checkbox_class');
 		$checked = ($value == 1)?'checked="checked"':'';
-		$input = '<input'.self::ats($attributes).' value="'.e($value).'" '.$checked.'/>';
+		$input = '<label class="'.config('barista.checkbox_class').'"><input'.self::ats($attributes).' value="'.e($value).'" '.$checked.'/>'.config('barista.checkbox_class').'</label>';
 		return $input;
 	}
-	public static function file ($name, $value, $attributes = null)
+
+	/**
+	 * Generate an HTML file input with path and url.
+	 * 
+	 * @param  string $name    
+	 * @param  string $value     
+	 * @param  array $attributes 
+	 * 
+	 * @return string      
+	 */
+	public static function file($name, $value, $attributes = null)
 	{
 		$file = "";
 		$file .= self::input($name , $value, $attributes);
@@ -191,25 +279,56 @@ class BaristaBuilder{
 		return $file;
 
 	}
-	public static function input ($name, $value, $attributes = null)
+
+	/**
+	 * Generate an HTML input element
+	 * 
+	 * @param  string $name     
+	 * @param  string $value    
+	 * @param  array $attributes
+	 *  
+	 * @return string  
+	 */
+	public static function input($name, $value, $attributes = null)
 	{
 		if(!isset($attributes['class']))
-			$attributes['class'] = 'form-control';
+			$attributes['class'] = config('barista.input_class');
 		$input = '<input'.self::ats($attributes).' value="'.e($value).'"/>';
 		return $input;
 	}
 
+	/**
+	 * Generate an HTML textarea element.
+	 *
+	 * @param  string $name       
+	 * @param  string $value 
+	 * @param  string $attributes 
+	 * 
+	 * @return string
+	 */
 	public static function textarea($name, $value, $attributes = null)
 	{
 		if(!isset($attributes['class']))
-			$attributes['class'] = 'form-control';
+			$attributes['class'] = config('barista.textarea_class');
 		return '<textarea'.self::ats($attributes).'>'.e($value).'</textarea>';
 	}
 
+	/**
+	 * Generate an HTML select element with given options.
+	 * 
+	 * @param  string $name      
+	 * @param  string $value    
+	 * @param  array $options  
+	 * @param  array $attributes 
+	 * 
+	 * @return string
+	 */
 	public static function select($name, $value , $options ,$attributes = null)
 	{	
 		$select = "";
-		$select .= '<select class="form-control" name="'.$attributes['name'].'">';
+		if(!isset($attributes['class']))
+			$attributes['class'] = config('barista.select_class');
+		$select .= '<select '.self::ats($attributes).'>';
 		foreach($options as $option)
 		{	
 			if(isset($value) && $value == $option->id)
@@ -221,28 +340,101 @@ class BaristaBuilder{
 		return $select;
 
 	}
+
+	/**
+	 * Generate an option for an HTML select.
+	 *
+	 * @param  string $name    
+	 * @param  string $value    
+	 * @param  string $selected 
+	 * 
+	 * @return string     
+	 */
 	public static function option($name, $value, $selected)
 	{
 		return '<option value="'.e($value).'"'.$selected.'>'. $name.'</option>';
-
 	}
+
+	/**
+	 * Generate an HTML link.
+	 * 
+	 * @param  string $url       
+	 * @param  string $value     
+	 * @param  array $attributes 
+	 * 
+	 * @return string   
+	 */
 	public static function link($url, $value, $attributes)
 	{
+		if(!isset($attributes['class']))
+			$attributes['class'] = config('barista.link_class');
 		return '<a href="'.$url.'"'.self::ats($attributes).'>'.e($value).'</a>';
 	}
+
+	/**
+	 * Generate an HTML label.
+	 * 
+	 * @param  string $name 
+	 * @param  string $value     
+	 * @param  array $attributes 
+	 * 
+	 * @return string        
+	 */
 	public static function label($name, $value ,$attributes = null)
 	{
+		if(!isset($attributes['class']))
+			$attributes['class'] = config('barista.label_class');
 		$value = (\Lang::has('general.'.e($value)))?trans('general.'.e($value)):e($value);
-		return '<label class="control-label" for="'.$name.'"'.self::ats($attributes).'>'.$value.'</label>';
+		$required = (isset($attributes['required']) && $attributes['required'] == "required" ) ? self::required():'';
+		return '<label for="'.$name.'"'.self::ats($attributes).'>'.$value.' '.$required.'</label>';
 	}
-	public static function required ($required = null)
+
+	/**
+	 * Generate required tag.
+	 * 
+	 * @return string
+	 */
+	public static function required ()
 	{
-		return ($required == 'required')?' <strong class="text-danger">*</strong>':'';
+		return '<strong class="'.config('barista.required_block_class').'">'.config('barista.required_tag').'</strong>';
 	}
+
+	/**
+	 * Generate error block.
+	 * 
+	 * @param  string $error  
+	 * @param  array $attributes
+	 * 
+	 * @return string       
+	 */
 	public static function error($error, $attributes = null)
 	{
+		if(!isset($attributes['class']))
+			$attributes['class'] = config('barista.error_block_class');
 		return '<span'.self::ats($attributes).'>'.$error.'</span>';
 	}	
+
+	/**
+	 * Generate help block.
+	 * 
+	 * @param  string $text  
+	 * @param  array $attributes
+	 * 
+	 * @return string       
+	 */
+	public static function help($text, $attributes = null)
+	{
+		if(!isset($attributes['class']))
+			$attributes['class'] = config('barista.help_block_class');
+		return '<span'.self::ats($attributes).'>'.$text.'</span>';
+	}	
+	/**
+	 * Convert array of attributes to HTML attribute string.
+	 * 
+	 * @param  array $attributes 
+	 * 
+	 * @return string
+	 */
 	public static function ats($attributes)
 	{
 		$string = "";
@@ -250,23 +442,23 @@ class BaristaBuilder{
 			return $string;
 		foreach($attributes as $key=>$value )
 		{
-			$string .= self::toAttribute($key, $value);
+			$string .= self::toHTMLAttribute($key, $value);
 		}
 		return $string;
 	}
-	private static function toAttribute($key, $value)
+
+	/**
+	 * Convert key, value pair to HTML Attribute string format.
+	 * 
+	 * @param  string $key  
+	 * @param  string $value 
+	 * 
+	 * @return string 
+	 */
+	private static function toHTMLAttribute($key, $value)
 	{
 		return ' '.$key.'="'.e($value).'"'; 
 	}
-	/*
-	  "id" => {#176 ▼
-    +"showName": "Id"
-    +"name": "id"
-    +"fieldType": "number"
-    +"editable": false
-    +"columnDefault": null
-    +"required": "NO"
-    */
 
   }
 
--- src/Barista/Publish/config/barista.php
+++ src/Barista/Publish/config/barista.php
@@ -1,5 +1,113 @@
 <?php 
 return [
-	'should_wrap_element' => true,
-	'element_wrapper' => 'form-group'
+	/*
+    |--------------------------------------------------------------------------
+    | Admin Route Prefix
+    |--------------------------------------------------------------------------
+    | 
+    | This is a prefix for admin dashboard. For example 'admin' will be the prefix for routes like /admin/users
+    | 
+    */
+	'prefix' => '',
+	/*
+    |--------------------------------------------------------------------------
+    | General Form Rules
+    |--------------------------------------------------------------------------
+    | 
+    | General rules for Barista to generate your HTML code properly.
+    | 
+    */
+ 	'should_group_form' => true,
+	/*
+    |--------------------------------------------------------------------------
+    | General Form Classes
+    |--------------------------------------------------------------------------
+    | 
+    | This part is all about general div classes will be added to your HTML.
+    | 
+    */
+	'group_class' => 'columns',
+	'group_label_class' => 'column is-4',
+	'group_input_class' => 'column is-8',
+
+	/*
+    |--------------------------------------------------------------------------
+    | General Form Classes
+    |--------------------------------------------------------------------------
+    | 
+    | This part is all about general div classes will be added to your HTML.
+    | 
+    */
+	'show_group_class' => 'columns',
+	'show_group_label_class' => 'column is-2',
+	'show_group_value_class' => 'column is-10',
+
+	'error_block_class' => 'help-block',
+	'information_block_class' => 'help-block',
+	//'required_block_class' => 'required',
+	'required_block_class' => 'tag is-warning',
+	'link_class' => '',
+	'select_class' => 'form-control',
+	'textarea_class' => 'form-control',
+	//'input_class' => 'form-control',
+	'input_class' => 'input',
+	'checkbox_class' => 'checkbox',
+	'label_class' => 'label',
+
+	/*
+    |--------------------------------------------------------------------------
+    | Button Classes
+    |--------------------------------------------------------------------------
+    | 
+    | Button classes for form and datatable.
+    | 
+    */	
+	//'btn_class' => 'btn',
+	'btn_class' => 'button',
+	//'btn_class_sm' => 'btn-sm',
+	'btn_sm_class' => 'is-small',
+	//'btn_primary' => 'btn-primary',
+	'btn_primary' => 'is-primary',
+	//'btn_success' => 'btn-success',
+	'btn_success' => 'is-success',
+	//'btn_danger' => 'btn-danger',
+	'btn_danger' => 'is-danger',
+	//'btn_info' => 'btn-info',
+	'btn_info' => 'is-info',
+	'btn_additional_class' => '',
+
+	/*
+    |--------------------------------------------------------------------------
+    | Table Classes
+    |--------------------------------------------------------------------------
+    | 
+    | This part is all about tables.
+    | 
+    */
+	'table_class' => 'table compact',
+	'thead_class' => 'thead-inverse',
+		//'btn_class' => 'btn',
+	'tbl_btn_class' => 'button',
+	//'btn_class_sm' => 'btn-sm',
+	'tbl_btn_sm_class' => 'is-small',
+	//'btn_primary' => 'btn-primary',
+	'tbl_btn_primary' => 'is-primary',
+	//'btn_success' => 'btn-success',
+	'tbl_btn_success' => 'is-success',
+	//'btn_danger' => 'btn-danger',
+	'tbl_btn_danger' => 'is-danger',
+	//'btn_info' => 'btn-info',
+	'tbl_btn_info' => 'is-info',
+	'tbl_btn_additional_class' => 'is-outlined',
+
+	/*
+    |--------------------------------------------------------------------------
+    | Barista Strings
+    |--------------------------------------------------------------------------
+    | 
+    | This part is all about strings.
+    | 
+    */	
+	'required_tag' => 'required',
+
 ];
